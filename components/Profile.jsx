da288cd752df413c40bab5161424547ce0531f7b
MuxammedL <layicovmuxammed@gmail.com>
2024-02-29 22:22:05 +0400

    complete edit and delete functionalities

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,8 +1,23 @@
-
-const Profile = () => {
+import PromptCard from "./PromptCard";
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} profile</span>
+      </h1>
+      <p className="desc text-left">{desc}</p>
+      <div className="mt-10 prompt_layout">
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleDelete={() => handleDelete && handleDelete(post)}
+            handleEdit={() => handleEdit && handleEdit(post)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Profile
+export default Profile;
