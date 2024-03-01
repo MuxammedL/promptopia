072da5d45313d1b326147fc433cb1a943ddde74f
MuxammedL <layicovmuxammed@gmail.com>
2024-03-01 08:46:36 +0400

    noStore

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,8 +1,10 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
+import { unstable_noStore as noStore } from "next/cache";
 
 export const GET = async (request) => {
   try {
+    noStore();
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
     return new Response(JSON.stringify(prompts), { status: 200 });
